b1b5895f1a484e2ef53cdc28d0f9511375e924ec
zaviany <44768003+zaviany@users.noreply.github.com>
2019-01-21 09:34:55 +0100

    Add files via upload

---
+++ zuo.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import aseegg as ag
+
+dane = pd.read_csv("dane04.csv", delimiter=',', engine='python')
+
+mrugniecia=dane["dwa"][:7552]
+cyfranamonitorze=dane["szesc"]
+
+czestProbkowania = 200
+czas = int(len(mrugniecia)/czestProbkowania)
+t = np.linspace(0, czas, czas * czestProbkowania)
+
+plt.plot(t, mrugniecia[0:czas*czestProbkowania])
+plt.show()
+
+
+przefiltrowany1= ag.pasmowozaporowy(mrugniecia, czestProbkowania, 49, 51)
+przefiltrowany2= ag.pasmowoprzepustowy(przefiltrowany1, czestProbkowania, 1, 50)
+
+plt.plot(t, przefiltrowany2[0:czas*czestProbkowania])
+plt.show()
+
+
+mors= []
+
+i=0
+
+while i <len(mrugniecia):
+    if mrugniecia[i] > 0.05:
+        mors.append(cyfranamonitorze[i])
+        i +=100
+    else:
+        i +=1
+
+print(mors)
